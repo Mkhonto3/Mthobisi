6bfeb9aaa838c9fab925d0b22212648a9d2fbae0
Mthobisi <mmkhonto3@gmail.com>
2017-03-23 13:41:57 +0200

    Created package.json and app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: 'a457eb4b-956d-4dff-addd-ffd83bad5031',
+    appPassword: 'fMVgJ4J9GoMHpachhOFZbb6'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
